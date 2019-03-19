57c2801c8b1f5179f76157d52c1d8ff7231c69b2
mistrue <cndyqjqtk@naver.com>
2019-03-19 11:59:10 +0900

    synchronization test

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace WebApplication.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
