481f2c973d7e8a7f2634cd9b6089bef427365d39
mistrue <cndyqjqtk@naver.com>
2019-03-26 18:45:33 +0900

    [fix] fix conflicts

--- DataStreamType/JsonUnit.cs
+++ DataStreamType/JsonUnit.cs
@@ -5,13 +5,18 @@ namespace DataStreamType
 {
     public class JsonList
     {
-        List<JsonUnit> list;
+        List<JsonUnit> sensorList;
+
+        public JsonList()
+        {
+            sensorList = new List<JsonUnit>();
+        }
 
         public int length
         {
             get
             {
-                return list.Count;
+                return sensorList.Count;
             }
         }
 
@@ -19,34 +24,41 @@ namespace DataStreamType
         {
             get
             {
-                if (index > list.Count)
+                if (index > sensorList.Count)
                 {
                     return null;
                 }
 
-                return list[index];
+                return sensorList[index];
             }
         }
 
         public void add(JsonUnit unit)
         {
-            list.Add(unit);
+            sensorList.Add(unit);
         }
 
         public void remove(JsonUnit unit)
         {
-            list.Remove(unit);
+            sensorList.Remove(unit);
+        }
+
+        public string serialize()
+        {
+            return JsonConvert.SerializeObject(this);
         }
     }
 
     public class JsonUnit
     {
         public string name;
+        public string type;
         public string value;
 
-        public JsonUnit(string _name, string _value)
+        public JsonUnit(string _name, string _type, string _value)
         {
             name = _name;
+            type = _type;
             value = _value;
         }
 
