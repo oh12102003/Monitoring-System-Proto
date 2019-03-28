88066cbd18f8e5f999b110bbadd1aeb8946dd0ba
Chang Uk Oh <cndyqjqtk@naver.com>
2019-03-29 01:14:58 +0900

    [add] (recipe page) js : add button function added

--- DataStreamType/Global.cs
+++ DataStreamType/Global.cs
@@ -6,6 +6,8 @@ namespace DataStreamType
     public class Global
     {
         public static IPEndPoint serverAddr = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
-        public static string jsonPath = Path.Combine(@"D:\백업 파일", "신입사원 2차 개발 과제", "Asynchornouse multi-socket client-server program", "WebApplication", "Sources");
+
+        public static string jsonPath = Path.Combine(@"C:\Users", "user", "Desktop", "Monitoring-System-Proto", "WebApplication", "Sources");
+        //public static string jsonPath = Path.Combine(@"D:\백업 파일", "신입사원 2차 개발 과제", "Asynchornouse multi-socket client-server program", "WebApplication", "Sources");
     }
 }
