ade5661b018ff29d63a3284d5bae191374f57b12
mistrue <cndyqjqtk@naver.com>
2019-03-29 17:42:37 +0900

    [recovery] recover projecft

--- DataStreamType/Global.cs
+++ DataStreamType/Global.cs
@@ -7,12 +7,7 @@ namespace DataStreamType
     {
         public static IPEndPoint serverAddr = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
 
-<<<<<<< HEAD
         //public static string jsonPath = Path.Combine(@"C:\Users", "user", "Desktop", "Monitoring-System-Proto", "WebApplication", "Sources");
         public static string jsonPath = Path.Combine(@"D:\백업 파일", "신입사원 2차 개발 과제", "Asynchornouse multi-socket client-server program", "WebApplication", "Sources");
-=======
-        public static string jsonPath = Path.Combine(@"C:\Users", "user", "Desktop", "Monitoring-System-Proto", "WebApplication", "Sources");
-        //public static string jsonPath = Path.Combine(@"D:\백업 파일", "신입사원 2차 개발 과제", "Asynchornouse multi-socket client-server program", "WebApplication", "Sources");
->>>>>>> 88066cbd18f8e5f999b110bbadd1aeb8946dd0ba
     }
 }
